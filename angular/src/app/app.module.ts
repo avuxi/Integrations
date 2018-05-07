73aadefe33912e0db1f3acc35524e4c767239683
Grisha Pushkov <grisha@push.org.ru>
2018-05-07 16:18:49 +0200

    rename to ngx

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MapComponent, LseComponent } from 'angular-avapi';
+import { MapComponent, LseComponent } from 'ngx-avapi';
 
 @NgModule({
   declarations: [
